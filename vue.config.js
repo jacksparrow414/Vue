3a1457e0258a969e3a098b8da6697d7b14377881
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2019-09-13 15:58:02 +0800

    修改代理路径

--- vue.config.js
+++ vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
       // change xxx-api/login => mock/login
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080/dhb/`,
+        target: `http://localhost:8080/baby/`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''

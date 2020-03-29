e02fbe4e02e57689a2626d90dd2e70744c108022
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-03-29 10:59:27 +0800

    修改默认密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -108,7 +108,7 @@
     return {
       loginForm: {
         username: 'admin',
-        password: '111111'
+        password: '123456'
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
